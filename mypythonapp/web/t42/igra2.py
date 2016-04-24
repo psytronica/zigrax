799140570e206091601e75fb95191bd64bb12210
zagga <zygopterix@gmail.com>
2016-04-24 10:59:12 +0300

    test 3

--- mypythonapp/web/t42/igra2.py
+++ mypythonapp/web/t42/igra2.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def main(t42):
+
     t42.viewport.init(
         {"id":"tea1", "width":"500px", "height": "300px", "x": "50px", "y": "20px",
         "content": 'Чай 1', "css": "border:1px dotted green; color:white",
